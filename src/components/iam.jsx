4e7272b17fe0f32fcc98dd3abac3c5c3b8afa490
vanshika704 <sharmavanshi704@gmail.com>
2024-09-15 00:40:08 +0530

    ffwrng

--- src/components/iam.jsx
+++ src/components/iam.jsx
@@ -5,7 +5,7 @@ export function FlipWordsDemo() {
 
   return (
     <div className="h-[40rem] flex absolute left-4 sm:left-10 items-center px-4">
-      <div className="text-4xl sm:text-2xl sm:mt-20 sm:ml-36 md:text-6xl md:ml-20 lg:text-7xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 text-center">
+      <div className="text-4xl sm:text-4xl sm:mt-32 sm:ml-36 md:text-6xl md:ml-20 md:mt-60 lg:text-7xl lg:mt-20 mx-auto font-normal text-neutral-600 dark:text-neutral-400 text-center">
         Hey Everyone !!!<br /> I am a
         <FlipWords words={words} /> <br />
         Developer
