2186746d49265c2de740db1436d5b710985d12d4
vanshika704 <sharmavanshi704@gmail.com>
2024-09-15 00:33:35 +0530

    ngf

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -12,11 +12,11 @@ export function ShootingStarsAndStarsBackgroundDemo() {
       <ShootingStars className="relative z-10" />
 
       {/* Content */}
-      <div className="absolute inset-0 flex flex-col lg:flex-row items-center justify-center p-4">
+      <div className="absolute inset-0 flex flex-col lg:flex-row p-4">
         <FlipWordsDemo />
 
         {/* Circular Image */}
-        <div className="w-48 h-48 sm:w-52 sm:h-52  md:w-72 md:h-72 lg:w-96 lg:h-96  mt-8 lg:mt-0 lg:ml-96 absolute right-10">
+        <div className="w-48 h-48 sm:w-52 sm:h-52 sm:mt-10  md:w-72 md:h-72 md:mt-10 lg:w-96 lg:h-96  mt-8 lg:mt-32 lg:ml-96 absolute right-10">
           <img
             src="src/assets/fotor-ai-20240914221650-removebg-preview (1).png" // Replace with your image path
             alt="Circular Image"
