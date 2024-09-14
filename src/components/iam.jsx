271d1fd4c9a1a0acdc27c3890f3aeb6553626bff
vanshika704 <sharmavanshi704@gmail.com>
2024-09-15 00:18:45 +0530

    ,dfs

--- src/components/iam.jsx
+++ src/components/iam.jsx
@@ -1,15 +1,14 @@
-
 import { FlipWords } from "./flipwords";
 
 export function FlipWordsDemo() {
   const words = ["React", "Flutter", "Firebase", "Node"];
 
   return (
-    <div className="h-[40rem] flex absolute left-10 items-center px-4">
-      <div className="text-7xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-      Hey Everyone !!!<br></br>  I am a
+    <div className="h-[40rem] flex absolute left-4 sm:left-10 items-center px-4">
+      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl mx-auto font-normal text-neutral-600 dark:text-neutral-400 text-center">
+        Hey Everyone !!!<br /> I am a
         <FlipWords words={words} /> <br />
-       Developer
+        Developer
       </div>
     </div>
   );
