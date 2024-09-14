b7b1be8d7df4d83071142f6ec37c1c8ff77ae350
vanshika704 <sharmavanshi704@gmail.com>
2024-09-14 23:58:44 +0530

    .

--- src/components/iam.jsx
+++ src/components/iam.jsx
@@ -5,9 +5,9 @@ export function FlipWordsDemo() {
   const words = ["React", "Flutter", "Firebase", "Node"];
 
   return (
-    <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        I am a
+    <div className="h-[40rem] flex absolute left-10 items-center px-4">
+      <div className="text-7xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+      Hey Everyone !!!<br></br>  I am a
         <FlipWords words={words} /> <br />
        Developer
       </div>
