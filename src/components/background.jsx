06c648f17b3d0af9d528b67f4236d04fc54ed9ab
vanshika704 <sharmavanshi704@gmail.com>
2024-09-15 00:23:11 +0530

    rkghhfdi

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -4,22 +4,23 @@ import { FlipWordsDemo } from "./iam";
 import ShootingStars from "./shooting-stars";
 import StarsBackground from "./stars-baackground";
 
-
 export function ShootingStarsAndStarsBackgroundDemo() {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
+      {/* Background components */}
       <StarsBackground className="absolute inset-0 w-full h-full" />
       <ShootingStars className="relative z-10" />
 
-      {/* Circular Image */}
-      <div className="absolute inset-0 flex items-center justify-center ">
-        <FlipWordsDemo/>
-        <div className="ml-96 w-96 h-96 absolute right-10">
+      {/* Content */}
+      <div className="absolute inset-0 flex flex-col lg:flex-row items-center justify-center p-4">
+        <FlipWordsDemo />
+
+        {/* Circular Image */}
+        <div className="w-48 h-48 md:w-72 md:h-72 lg:w-96 lg:h-96  mt-8 lg:mt-0 lg:ml-96 absolute right-32">
           <img
             src="src/assets/fotor-ai-20240914221650-removebg-preview (1).png" // Replace with your image path
             alt="Circular Image"
-          
-            className="rounded-full"
+            className="rounded-full object-cover w-full h-full"
           />
         </div>
       </div>
