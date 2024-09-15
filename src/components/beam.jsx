eed574117e7d7740d7823a1573f7130c0df00cc5
vanshika704 <sharmavanshi704@gmail.com>
2024-09-15 11:56:32 +0530

    vouv

--- src/components/beam.jsx
+++ src/components/beam.jsx
@@ -6,21 +6,21 @@ import { FloatingDockDemo } from "../components/navbar1";
 
 const TracingBeamDemo = () => {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
+    <div className="relative w-full h-screen overflow-hidden">
       {/* ShootingStarsAndStarsBackgroundDemo positioned to cover the entire screen */}
       <div className="absolute inset-0 z-0">
         <ShootingStarsAndStarsBackgroundDemo />
       </div>
 
       {/* TracingBeam positioned absolutely on the left side */}
-      <TracingBeam className="absolute left-20 top-0 h-full z-50 w-10 "> </TracingBeam>{/* Updated z-30 */}
-        <div className="px-6 h-full flex flex-col mt-10 z-30"> {/* Ensure content also has z-30 */}
-          {/* FloatingDockDemo component positioned above the stars background */}
-          <div className="relative mt-2 z-40"> {/* Higher z-index for FloatingDockDemo */}
-            <FloatingDockDemo />
-          </div>
-        </div>
+      <TracingBeam className="fixed left-20 top-0 h-full z-50 w-10" /> {/* Use fixed positioning for visibility */}
       
+      <div className="px-6 h-full flex flex-col mt-10 relative z-30"> {/* Ensure content also has z-30 */}
+        {/* FloatingDockDemo component positioned above the stars background */}
+        <div className="relative mt-2 z-40"> {/* Higher z-index for FloatingDockDemo */}
+          <FloatingDockDemo />
+        </div>
+      </div>
     </div>
   );
 };
