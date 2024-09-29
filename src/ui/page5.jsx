826e08e771cfa9da87b603f8f48d97bb4f14a05a
vanshika704 <sharmavanshi704@gmail.com>
2024-09-29 12:55:32 +0530

    huj

--- src/ui/page5.jsx
+++ src/ui/page5.jsx
@@ -18,7 +18,7 @@ function Page5() {
             // marginTop:'200px' // Set your desired hr height
           }}
         />
-     <div className="relative w-full flex flex-col lg:flex-row bg-neutral-900 lg:h-screen sm:h-[200vh]" >
+     <div className="relative w-screen flex flex-col lg:flex-row bg-neutral-900 lg:h-screen sm:h-[200vh]" >
         {/* Background components */}
         <StarsBackground className="absolute inset-0 w-full h-full" />
         <ShootingStars className="relative z-10" />
