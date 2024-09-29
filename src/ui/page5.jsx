a8e131b06117f750f7bad9679a995fd2f6c725ca
vanshika704 <sharmavanshi704@gmail.com>
2024-09-29 12:47:48 +0530

    jnm

--- src/ui/page5.jsx
+++ src/ui/page5.jsx
@@ -18,12 +18,12 @@ function Page5() {
             // marginTop:'200px' // Set your desired hr height
           }}
         />
-     <div className="relative w-full flex flex-col lg:flex-row dark:bg-neutral-900 lg:h-screen sm:h-[200vh]" >
+     <div className="relative w-full flex flex-col lg:flex-row lg:h-screen sm:h-[200vh]" >
         {/* Background components */}
         <StarsBackground className="absolute inset-0 w-full h-full" />
         <ShootingStars className="relative z-10" />
      
-        <div className="absolute inset-0 flex justify-center bg-transparent lg:items-center z-20 sm:mt-96 lg:mt-0">
+        <div className="absolute inset-0 flex justify-center  lg:items-center z-20 sm:mt-0 lg:mt-0">
         {/* LensDemoThird positioned above the background */}
        <Social/>
        </div>
